c018cbfdd9f696f7348ccdd92c4ca7faf810671a
crosslandwa <crossland.w.a@gmail.com>
2016-08-17 14:36:52 +0100

    Update version to 1.2.1. Separate lint and test commands (to run linting without running tests). Update linting to use standard js standards, and format code to adhere to linting rules.

--- Scheduling.js
+++ Scheduling.js
@@ -1,87 +1,87 @@
-'use strict';
+'use strict'
 
-const EventEmitter = require('events'),
-    util = require('util');
+const EventEmitter = require('events')
+const util = require('util')
 
-function Repeater(inTheFuture, initialInterval) {
-    EventEmitter.call(this);
+function Repeater (inTheFuture, initialInterval) {
+  EventEmitter.call(this)
 
-    let recursiveInTheFuture = function(callback) {
-        return inTheFuture(() => {
-            if (_isScheduling) {
-                callback();
-                _cancel = recursiveInTheFuture(callback);
-            }
-        }, _interval);
-    };
+  let recursiveInTheFuture = function (callback) {
+    return inTheFuture(() => {
+      if (_isScheduling) {
+        callback()
+        _cancel = recursiveInTheFuture(callback)
+      }
+    }, _interval)
+  }
 
-    let _isScheduling = false,
-        _interval = createInterval(initialInterval),
-        _cancel = noAction,
-        repeater = this;
+  let _isScheduling = false
+  let _interval = createInterval(initialInterval)
+  let _cancel = noAction
+  let repeater = this
 
-    this.updateInterval = function (newInterval) {
-        _interval = createInterval(newInterval);
-        repeater.reportInterval();
-        return repeater;
-    };
+  this.updateInterval = function (newInterval) {
+    _interval = createInterval(newInterval)
+    repeater.reportInterval()
+    return repeater
+  }
 
-    this.start = function(callback) {
-        if (_isScheduling) return;
-        _isScheduling = true;
-        callback();
-        _cancel = recursiveInTheFuture(callback);
-    };
+  this.start = function (callback) {
+    if (_isScheduling) return
+    _isScheduling = true
+    callback()
+    _cancel = recursiveInTheFuture(callback)
+  }
 
-    this.stop = function() {
-        _isScheduling = false;
-        _cancel();
-        _cancel = noAction;
-    };
+  this.stop = function () {
+    _isScheduling = false
+    _cancel()
+    _cancel = noAction
+  }
 
-    this.reportInterval = function() {
-        repeater.emit('interval', _interval);
-    };
+  this.reportInterval = function () {
+    repeater.emit('interval', _interval)
+  }
 }
-util.inherits(Repeater, EventEmitter);
+util.inherits(Repeater, EventEmitter)
 
-function Scheduling(context) {
-    let scheduling = this;
+function Scheduling (context) {
+  let scheduling = this
 
-    let inTheFutureTight = function(callback, when) {
-        let source = context.createBufferSource(),
-            now = context.currentTime,
-            thousandth = context.sampleRate / 1000,
-            scheduled_at = now + (createInterval(when).toMs() / 1000) - 0.001;
-        // a buffer length of 1 sample doesn't work on IOS, so use 1/1000th of a second
-        let buffer = context.createBuffer(1, thousandth, context.sampleRate);
-        source.addEventListener('ended', callback);
-        source.buffer = buffer;
-        source.connect(context.destination);
-        source.start(scheduled_at);
+  let inTheFutureTight = function (callback, when) {
+    let source = context.createBufferSource()
+    let now = context.currentTime
+    let thousandth = context.sampleRate / 1000
+    let scheduledAt = now + (createInterval(when).toMs() / 1000) - 0.001
+    // a buffer length of 1 sample doesn't work on IOS, so use 1/1000th of a second
+    let buffer = context.createBuffer(1, thousandth, context.sampleRate)
+    source.addEventListener('ended', callback)
+    source.buffer = buffer
+    source.connect(context.destination)
+    source.start(scheduledAt)
 
-        return function cancel() {
-            source.removeEventListener('ended', callback);
-            source.stop();
-        };
-    };
+    return function cancel () {
+      source.removeEventListener('ended', callback)
+      source.stop()
+    }
+  }
 
-    this.inTheFuture = context ? inTheFutureTight : inTheFutureLoose;
+  this.inTheFuture = context ? inTheFutureTight : inTheFutureLoose
 
-    this.Repeater = function(initialInterval) {
-        return new Repeater(scheduling.inTheFuture, initialInterval);
-    };
-}
-
-function inTheFutureLoose(callback, when) {
-    let timer = setTimeout(callback, createInterval(when).toMs());
-    return function cancel() { clearTimeout(timer); };
+  this.Repeater = function (initialInterval) {
+    return new Repeater(scheduling.inTheFuture, initialInterval)
+  }
 }
 
-function noAction() {}
-
-function createInterval(candidate) {
-    return (candidate && (typeof candidate.toMs === 'function')) ? candidate : { toMs: function() { return candidate; }};
+function inTheFutureLoose (callback, when) {
+  let timer = setTimeout(callback, createInterval(when).toMs())
+  return function cancel () { clearTimeout(timer) }
 }
 
-module.exports = function(context) { return new Scheduling(context); };
+function noAction () {}
+
+function createInterval (candidate) {
+  return (candidate && (typeof candidate.toMs === 'function')) ? candidate : { toMs: function () { return candidate } }
+}
+
+module.exports = function (context) { return new Scheduling(context) }
