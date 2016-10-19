a4d0525f7ca039b2958bb72aa442ec24b57528cb
crosslandwa <crossland.w.a@gmail.com>
2016-10-19 14:17:37 +0100

    Remove WebAudioApi generated clicks from metronome as Firefox does not like doing very short envelopes - decided to make clients implement audible metronome sounds rather than keep that in the library

--- src/Metronome.js
+++ src/Metronome.js
@@ -8,40 +8,12 @@ const { clamp } = require('ramda')
 const between1And16 = clamp(1, 16)
 const bpmBeatLength = (bpm) => new BPM(bpm).beatLength()
 
-function Metronome (Repeater, context, initialNumberOfBeats, initialBPM) {
+function Metronome (Repeater, initialNumberOfBeats, initialBPM) {
   EventEmitter.call(this)
   let metronome = this
   let numberOfBeats = between1And16(initialNumberOfBeats)
   let repeater = Repeater(bpmBeatLength(initialBPM))
   let count = -1
-  let gain
-
-  function click (f) {
-    gain.gain.value = 0.25 // -6dB
-
-    let osc = context.createOscillator()
-    osc.frequency.value = f
-    osc.frequency.exponentialRampToValueAtTime(10, 0.005)
-    osc.start()
-    osc.stop(context.currentTime + 0.005)
-    osc.connect(gain)
-    gain.gain.linearRampToValueAtTime(0, 0.0005)
-  }
-
-  function makeAccentClick () {
-    click(1200)
-  }
-
-  function makeTickClick () {
-    click(900)
-  }
-
-  if (context) {
-    gain = context.createGain()
-    gain.connect(context.destination)
-    metronome.on('accent', makeAccentClick)
-    metronome.on('tick', makeTickClick)
-  }
 
   function tick () {
     count = ++count % numberOfBeats
@@ -69,12 +41,6 @@ function Metronome (Repeater, context, initialNumberOfBeats, initialBPM) {
 
   this.updateBPM = function (newBPM) {
     repeater.updateInterval(bpmBeatLength(newBPM))
-  }
-
-  this.suppressClick = function () {
-    metronome.removeListener('accent', makeAccentClick)
-    metronome.removeListener('tick', makeTickClick)
-    return metronome
   }
 
   repeater.on('interval', (event) => {
